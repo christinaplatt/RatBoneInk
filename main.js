8474702a502631ce433bc7b11febc28db8bfcd55
christina platt <christinamarieplatt@gmail.com>
2016-01-31 06:56:36 -0500

    added more images with links to etsy store

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	console.log("test");
 	$(".butts").hover(makeBigger, returnToOriginalSize);
 		function makeBigger(){
-		$(this).css({height: '+=50%', width: '+=50%'});
+		$(this).css({height: '+=60%', width: '+=60%'});
 	}
 	function returnToOriginalSize(){
 		$(this).css({height:"", width: ""});
